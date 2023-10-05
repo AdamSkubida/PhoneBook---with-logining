4f6eb7d962ec203cdce46c2780ee255ff1bc1400
AdamSkubida <adasiow.s@gmail.com>
2023-10-05 22:32:11 +0200

    featureRedux/actions, reducers, selectors, slice, store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { Provider } from 'react';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="goit-react-hw-08-phonebook">
